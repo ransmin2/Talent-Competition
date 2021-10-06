4312ed1ab6cda50122a8e9ac1e6760f618fb6dd8
ransmin2 <50533122+ransmin2@users.noreply.github.com>
2021-10-06 10:59:20 +0100

    Update EmployerProfile.jsx

--- Profile/EmployerProfile.jsx
+++ Profile/EmployerProfile.jsx
@@ -52,7 +52,10 @@ export default class EmployeeProfile extends React.Component {
     loadData() {
         var cookies = Cookies.get('talentAuthToken');
         $.ajax({
-            url: 'http://localhost:60290/profile/profile/getEmployerProfile',
+             /*url: 'http://localhost:60290/profile/profile/getEmployerProfile',*/
+            url: 'https://talentappwebapp1.azurewebsites.net/profile/profile/getEmployerProfile',
+
+            
             headers: {
                 'Authorization': 'Bearer ' + cookies,
                 'Content-Type': 'application/json'
